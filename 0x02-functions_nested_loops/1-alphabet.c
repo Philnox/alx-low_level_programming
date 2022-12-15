323ac6b6a20b18209cbe28ba4dce35215e5f9bb7
Philnox <felixnwandu@gmail.com>
2022-12-15 02:21:12 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet - print letters a to z
+*
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
