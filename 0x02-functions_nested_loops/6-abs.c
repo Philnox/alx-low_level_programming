be1945541dbdd912f29aee56ca64e2777e711847
Philnox <felixnwandu@gmail.com>
2022-12-15 02:49:14 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - checks number
+ * @r: integer receipt by main.
+ * Return: Returns r when it is positive absolute value,
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
