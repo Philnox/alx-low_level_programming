34083fd03a644c73a09b8c1e7dc50e6a03cb0500
Philnox <felixnwandu@gmail.com>
2022-12-21 14:49:26 -0800

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase letters of a string to uppercase
+ * @s: the string the change
+ *
+ * Return: pointer to the resulting string
+ */
+char *string_toupper(char *s)
+{
+	char *c;
+
+	for (c = s; *c; ++c)
+	{
+		if ('a' <= *c && *c <= 'z')
+			*c -= 'a' - 'A';
+	}
+
+	return (s);
+}
