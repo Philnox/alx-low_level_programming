c3924242d9c9727155d980fa8c784fe98782061a
Philnox <felixnwandu@gmail.com>
2022-12-21 14:58:08 -0800

    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * print_number - print an integer
+ * @n: the integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n > -1)
+	{
+		if (n > 9)
+			print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
+	else
+	{
+		_putchar('-');
+		if (n < -9)
+			print_number(n / -10);
+		_putchar('0' - n % 10);
+	}
+}
