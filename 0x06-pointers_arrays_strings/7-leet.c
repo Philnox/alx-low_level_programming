c47894bd420e3741a0f144642390b32d1d701faa
Philnox <felixnwandu@gmail.com>
2022-12-21 14:54:15 -0800

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * leet - encode a string into 1337.
+ * @s: the string to encode
+ *
+ * Description: Characters are encoded as follows:
+ * {'a', 'A'} -> '4'
+ * {'e', 'E'} -> '3'
+ * {'o', 'O'} -> '0'
+ * {'t', 'T'} -> '7'
+ * {'l', 'L'} -> '1'
+ *
+ * Return: pointer to the encoded string
+ */
+char *leet(char *s)
+{
+	char *c;
+	char map[15] = {
+		'a', 'A', '4',
+		'e', 'E', '3',
+		'o', 'O', '0',
+		't', 'T', '7',
+		'l', 'L', '1'
+	};
+	int i;
+
+	for (c = s; *c; ++c)
+	{
+		for (i = 0; i < 15; i += 3)
+		{
+			if (*c == map[i] || *c == map[i + 1])
+				*c = map[i + 2];
+		}
+	}
+
+	return (s);
+}
