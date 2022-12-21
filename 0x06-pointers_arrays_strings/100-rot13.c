fce7484c36dc14472e85b52e0f2dcbf6479c8a07
Philnox <felixnwandu@gmail.com>
2022-12-21 14:56:30 -0800

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - encode a string using rot13
+ * @s: the string to encode
+ *
+ * Description: Each letter is replaced with the 13th letter after it.
+ *
+ * Return: pointer to the encoded string
+ */
+char *rot13(char *s)
+{
+	char *c;
+	char shift;
+
+	for (c = s; *c; ++c)
+	{
+		shift = 'A' + (*c & 32);
+		if (('a' <= *c && *c <= 'z') || ('A' <= *c && *c <= 'Z'))
+			*c = (*c - shift + 13) % 26 + shift;
+	}
+
+	return (s);
+}
