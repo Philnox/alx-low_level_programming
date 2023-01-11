ef609943b20b54d8fc1c39c72b0198b213f2bc17
Philnox <felixnwandu@gmail.com>
2023-01-11 03:01:53 -0800

    Task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: MAIN.h
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -11,5 +11,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
 #endif
