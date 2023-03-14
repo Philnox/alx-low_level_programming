99a838d461a90e46da7c4c3342cfe6873f2f8c91
Philnox <Philnox>
2023-03-14 08:13:54 -0700

    Task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
+/*
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x12-more_singly_linked_lists directory.
+ */
+
 #include <stdlib.h>
 
 /**
@@ -10,12 +14,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for alx project
+ * for Holberton project
  */
 typedef struct listint_s
 {
- int n;
- struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -34,4 +38,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* LISTS_H */
